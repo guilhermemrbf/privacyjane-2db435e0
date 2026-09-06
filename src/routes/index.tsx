707915ad1f54f0ce2569ca0b923074db62a75af3
guilhermemrbf <131459170+guilhermemrbf@users.noreply.github.com>
2026-09-06 15:50:46 -0300

    Atualiza descrição da Jane

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,11 @@ export const Route = createFileRoute("/")({
   head: () => ({
     meta: [
       { title: "Privacy | Jane" },
-      { name: "description", content: "Acesso ao conteúdo exclusivo." },
+      {
+        name: "description",
+        content:
+          "Sou a Jane, mais conhecida como coroa do hot 💋\n\nDentro do meu VIP você vai encontrar:\n❤️‍🔥 +234 vídeos exclusivos só pra você, Gozando bem gostoso sozinha, Chamadas privadinhas, Punheta guiada com minha voz, Meu WhatsApp pessoal além de conversar comigo diretamente pelo chat",
+      },
     ],
   }),
   component: Index,
